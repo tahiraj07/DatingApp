6723fe8eb1625f209aa4f3fe603f1477055642d6
islam.tahiraj <islam.tahiraj@hotmail.com>
2020-03-16 15:28:42 +0100

    finished

--- DATINGAPP-SPA/src/app/_services/user.service.ts
+++ DATINGAPP-SPA/src/app/_services/user.service.ts
@@ -5,6 +5,7 @@ import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { PaginateResult } from '../_models/pagination';
 import { map } from 'rxjs/operators';
+import { Message } from '../_models/message';
 
  
 
@@ -46,11 +47,11 @@ constructor(private http: HttpClient) { }
           map(response => {
             paginatedResult.result = response.body;
             if (response.headers.get('Pagination') != null) {
-              paginatedResult.pagination =  JSON.parse(response.headers.get('Pagination'))
+              paginatedResult.pagination = JSON.parse(response.headers.get('Pagination'))
             }
             return paginatedResult;
           })
-        )
+        );
   }
 
   getUser(id): Observable<User> {
@@ -71,5 +72,47 @@ constructor(private http: HttpClient) { }
 
   sendLike(id: number, recipientId: number) {
     return this.http.post(this.baseUrl + 'user/' + id + '/like/' + recipientId, {});
+  }
+
+  getMessages(id: number, page?, itemsPerPage?, messageContainer?) {
+    const paginatedResult: PaginateResult<Message[]> = new PaginateResult<Message[]>();
+
+    let params = new HttpParams();
+
+    params = params.append('MessageContainer', messageContainer);
+
+    if (page != null && itemsPerPage != null) {
+      params = params.append('pageNumber', page);
+      params = params.append('pageSize', itemsPerPage);
+    }
+
+    return this.http.get<Message[]>(this.baseUrl + 'user/' + id + '/messages', {observe: 'response', params})
+      .pipe(
+        map(response => {
+          paginatedResult.result = response.body;
+          if (response.headers.get('Pagination') != null) {
+            paginatedResult.pagination = JSON.parse(response.headers.get('Pagination'));
+          }
+
+          return paginatedResult;
+        })
+      );
+  }
+
+  getMessageThread(id: number, recipientId: number) {
+    return this.http.get<Message[]>(this.baseUrl + 'user/' + id + '/messages/thread/' + recipientId);
+  }
+
+  sendMessage(id: number, message: Message) {
+    return this.http.post(this.baseUrl + 'user/' + id + '/messages', message);
+  }
+
+  deleteMessage(id: number, userId: number) {
+    return this.http.post(this.baseUrl + 'user/' + userId + '/messages/' + id, {});
+  }
+
+  markAsRead(userId: number, messageId: number) {
+    this.http.post(this.baseUrl + 'user/' + userId + '/messages/' + messageId + '/read', {})
+    .subscribe();
   }
 }
